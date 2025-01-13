93c4f949da0c9f28d94041204a2d804f33b0dfe1
Tendai E. Mhona <mhonatendai@gmail.com>
2025-01-13 17:45:42 -0500

    WIP

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,23 +7,23 @@ const Login = () => {
             <div className="title">
                 <span>Mission Metrics</span>
             </div>
-            <p className='title_para'>Please enter your details to sign in.</p>
-
-            <form action="#">
-                <div className="row">
-                    {/* <i className="fas fa-user"></i> */}
-                    <input type="text" placeholder="Enter your email..." required />
-                </div>
-                <div className="row">
-                    {/* <i className="fas fa-lock"></i> */}
-                    <input type="password" placeholder="Password" required />
-                </div>
-                <div className="pass"><a href="#">Forgot password?</a></div>
-                <div className="row button">
-                    <input type="submit" value="Login" />
-                </div>
-                <div className="signup-link">New ? <a href="#">Register here</a></div>
-            </form>
+            <div>
+                <form action="#">
+                    <div className="row">
+                        {/* <i className="fas fa-user"></i> */}
+                        <input type="text" placeholder="Enter your email..." required />
+                    </div>
+                    <div className="row">
+                        {/* <i className="fas fa-lock"></i> */}
+                        <input type="password" placeholder="Password" required />
+                    </div>
+                    <div className="pass"><a href="#">Forgot password?</a></div>
+                    <div className="row button">
+                        <input type="submit" value="Login" />
+                    </div>
+                    <div className="signup-link">New ? <a href="#">Register here</a></div>
+                </form>
+            </div>
         </div>
     )
 }
