e20f46dee21c15f5c759c35212d171c3e946bb54
Tendai E. Mhona <mhonatendai@gmail.com>
2025-01-14 00:29:40 -0500

    Installed router dom, styled Login.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Login from './components/Login'
+import React from 'react';
+import Login from './components/Login';
+import './App.css'
 
 const App = () => {
   return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import './LoginForm.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div className='wrapper'>
-            <div className="title">
-                <span>Mission Metrics</span>
-            </div>
             <div>
+                <h3 className='title'>Mission Metrics</h3>
                 <form action="#">
-                    <div className="row">
-                        {/* <i className="fas fa-user"></i> */}
+                    <div className="input-box">
                         <input type="text" placeholder="Enter your email..." required />
                     </div>
-                    <div className="row">
-                        {/* <i className="fas fa-lock"></i> */}
+                    <div className="input-box">
                         <input type="password" placeholder="Password" required />
                     </div>
-                    <div className="pass"><a href="#">Forgot password?</a></div>
-                    <div className="row button">
-                        <input type="submit" value="Login" />
+                    <div className="input-box">
+                        <button type="button" class="btn btn-primary">Login</button>
                     </div>
                     <div className="signup-link">New ? <a href="#">Register here</a></div>
                 </form>
