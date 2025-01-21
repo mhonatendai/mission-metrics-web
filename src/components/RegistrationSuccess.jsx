fc5e54781529d59b84562fe2531d64621417a74d
Tendai E. Mhona <mhonatendai@gmail.com>
2025-01-20 22:55:48 -0500

    Login on successful registration.

--- src/components/RegistrationSuccess.jsx
+++ src/components/RegistrationSuccess.jsx
@@ -5,6 +5,7 @@ const RegistrationSuccess = ({ message, response }) => {
         <div>
             <h2>Success!</h2>
             <p>{message}</p>
+            <p>Go to <Link to="/login">Login Page</Link></p>
             <pre>{JSON.stringify(response, null, 2)}</pre>
         </div>
     );
