82cd90694f6ee3fa1fb591e30bc5198fe7f1586f
Tendai E. Mhona <mhonatendai@gmail.com>
2025-02-04 19:02:50 -0500

    Home and settings pages

---
+++ src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div style={{ marginLeft: '220px', padding: '20px' }}>
+            <h2>Settings Page</h2>
+            <p>Here you can modify your settings.</p>
+        </div>
+    );
+};
+
+export default Settings;
